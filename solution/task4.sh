fa89fa8dbc335b64d665b9e3488b8ffbb32ccd06
iwawaq <poshchadite@gmail.com>
2025-09-16 19:52:40 +0700

    Update solution

--- solution/task4.sh
+++ solution/task4.sh
@@ -1,5 +1,4 @@
 cd data
 cp blok.txt blok_kids_edition.txt
 
-sed 's/Живи/Не спи/g' blok_kids_edition.txt
-sed 's/Умрёшь/Уснёшь/g' blok_kids_edition.txt
+sed 's/Живи/Не спи/g; s/Умрёшь/Уснёшь/g' blok_kids_edition.txt
